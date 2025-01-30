2850c7d8c2179c1909ac9a12991b4816b1f6c710
George Morales <georgepm20002@gmail.com>
2025-01-30 17:28:52 -0500

    TLS cert check removed from Master Container settings

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -6,11 +6,14 @@ docker network create jenkins
 
 # Run Jenkins with Blueocean UI
 docker run --name jenkins-blueocean --restart=on-failure --detach \
-  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
-  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --network jenkins \
+  --env DOCKER_HOST="tcp://172.18.0.3:2375" \
+  --env DOCKER_TLS_VERIFY="0" \
+  --env DOCKER_CERT_PATH="" \
   --volume jenkins-data:/var/jenkins_home \
-  --volume jenkins-docker-certs:/certs/client:ro \
-  --publish 8080:8080 --publish 50000:50000 myjenkins-blueocean:latest
+  --publish 8080:8080 --publish 50000:50000 \
+  myjenkins-blueocean:latest
+
 
 # Get admin password
 docker exec jenkins-blueocean cat /var/jenkins_home/secrets/initialAdminPassword
